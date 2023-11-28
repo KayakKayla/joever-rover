275b640ea34e4496a5753c4653dda974502052be
Kayla <kayla76815@gmail.com>
2023-11-28 15:06:39 -0500

    initial commit

---
+++ openjoe.py
@@ -0,0 +1,16 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+
+while True:
+	ret, frame = cap.read()
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+	for (x,y,w,h) in faces:
+		cv2.rectangle(frame,(x,y),(x+w, y+h), (255,0,0),5)
+	cv2.imshow('frame',frame)
+	if cv2.waitKey(1) == ord('q'):
+		break
+cap.release()
+cv2.destroyAllWindows()
